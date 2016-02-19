edcfdea11836681e2aec19ffb8bea3b67b0c663b
philippsimon <github@philippsimon.de>
2016-02-19 11:46:46 +0100

    add merge debug script with console replace

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,7 +23,7 @@ module.exports = function(grunt) {
       ]
     },
     'string-replace': {
-      'feature-detects': {
+      console: {
         files: [{
           expand: true,
           src: 'feature-detects/**/*.js'
